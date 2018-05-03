3a3c53d7baf3d0977218164161c3a14b72d864e1
Александр Гаврилов <itgavart@gmail.com>
2018-05-03 14:51:05 +0300

    added SalesforceService

---
+++ salesforce.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+declare class Visualforce {
+  static remoting: { Manager: { invokeAction: any } };
+}
+
+@Injectable()
+export class SalesforceService {
+
+  constructor() { }
+
+  public getSFResourse = (path: string) => `${window['_VfResourses']}/${path}`;
+
+  public callRemote(methodName: string, params: string[], resolve, reject, config?: any) {
+    Visualforce.remoting.Manager.invokeAction(
+      methodName,
+      ...params,
+      function (result, event) {
+        try {
+          result = JSON.parse(result);
+        } catch (error) {
+          reject({
+            type: 'danger',
+            message: error
+          });
+        }
+        if (event.status) {
+          if (result.type === 'success') {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        } else {
+          reject(result);
+        }
+      },
+      config || { buffer: false, escape: false }
+    );
+  }
+
+}
